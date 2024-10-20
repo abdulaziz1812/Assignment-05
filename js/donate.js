7352befaecd39f6512d3aaecef1d1251040fd0ac
abdulaziz1812 <abdulaziz.abdulmannan@gmail.com>
2024-10-21 00:12:10 +0600

    card-2 done

--- js/donate.js
+++ js/donate.js
@@ -8,41 +8,34 @@ document.getElementById('btn-donation-noakhali')
         const titleChange = document.getElementById('title-noakhali');
         const cardCurrentBalance = document.getElementById('balance-noakhali')
 
+       donationBtnClick(donation,balance,cardBalance,titleChange,cardCurrentBalance)
+        
+    })
 
-        if (donation > balance || isNaN(donation) || donation <= 0 || donation === "") {
-            alert('Invalid Donation Amount');
-            return;
-        }
-        else {
-            // balance update
-            const newBalance = balance - donation;
-            document.getElementById('balance').innerText = newBalance;
+document.getElementById('btn-donation-feni')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
 
-            // Card balance update
-            const newCardBalance = cardBalance + donation;
-            cardCurrentBalance.innerText = newCardBalance;
+        const donation = inputAmountById('input-donation-feni');
+        const balance = textAmountToNumberById('balance');
+        const cardBalance = textAmountToNumberById('balance-feni');
+        const titleChange = document.getElementById('title-feni');
+        const cardCurrentBalance = document.getElementById('balance-feni')
 
-            // modal
-            document.getElementById('my_modal_1').showModal();
+       donationBtnClick(donation,balance,cardBalance,titleChange,cardCurrentBalance)
+        
+    })
+    
+document.getElementById('btn-donation-feni')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
 
-            // history
-            titleChange.innerText = titleChange.innerText.replace("Donate", "Donated");
-            const newTitle = titleChange.innerText;
+        const donation = inputAmountById('input-donation-feni');
+        const balance = textAmountToNumberById('balance');
+        const cardBalance = textAmountToNumberById('balance-feni');
+        const titleChange = document.getElementById('title-feni');
+        const cardCurrentBalance = document.getElementById('balance-feni')
 
-            const newTransaction = document.createElement('p');
-            newTransaction.classList.add('border', 'p-8', 'rounded-xl')
-            console.log(newTransaction);
-            const date = new Date();
-            
-
-            newTransaction.innerHTML = `<strong>${donation} Taka is ${newTitle}</strong>
-            <br>
-            Date: ${date}`;
-            
-           
-            document.getElementById('history-container').appendChild(newTransaction);
-
-            titleChange.innerText = titleChange.innerText.replace("Donated", "Donate");
-
-        }
+       donationBtnClick(donation,balance,cardBalance,titleChange,cardCurrentBalance)
+        
     })
--- js/utilities.js
+++ js/utilities.js
@@ -25,3 +25,42 @@ function textAmountToNumberById(id) {
     const textToNumber = parseFloat(textAmount);
     return textToNumber;
 }
+
+// donation if section
+function donationBtnClick(donation,balance,cardBalance,titleChange,cardCurrentBalance){
+    if (donation > balance || isNaN(donation) || donation <= 0 || donation === "") {
+        alert('Invalid Donation Amount');
+        return;
+    }
+    else {
+        // balance update
+        const newBalance = balance - donation;
+        document.getElementById('balance').innerText = newBalance;
+
+        // Card balance update
+        const newCardBalance = cardBalance + donation;
+        cardCurrentBalance.innerText = newCardBalance;
+
+        // modal
+        document.getElementById('my_modal_1').showModal();
+
+        // history
+        titleChange.innerText = titleChange.innerText.replace("Donate", "Donated");
+        const newTitle = titleChange.innerText;
+
+        const newTransaction = document.createElement('p');
+        newTransaction.classList.add('border', 'p-8', 'rounded-xl')
+        const date = new Date();
+        
+
+        newTransaction.innerHTML = `<strong>${donation} Taka is ${newTitle}</strong>
+        <br>
+        Date: ${date}`;
+        
+       
+        document.getElementById('history-container').appendChild(newTransaction);
+
+        titleChange.innerText = titleChange.innerText.replace("Donated", "Donate");
+
+    }
+}
