5b9379082a7a742c1793a0336762f2ec966c2845
abdulaziz1812 <abdulaziz.abdulmannan@gmail.com>
2024-10-21 13:23:51 +0600

    button fixed

--- js/button.js
+++ js/button.js
@@ -1,14 +1,14 @@
 // home ==> blog page
 if (document.getElementById("blog-button")) {
     document.getElementById("blog-button").addEventListener("click", function () {
-        window.location.href = "blog.html";
+        window.location.href = "/blog.html";
     });
 }
 
 // blog ==> home page
 if (document.getElementById("home-button")) {
     document.getElementById("home-button").addEventListener("click", function () {
-        window.location.href = "index.html";
+        window.location.href = "/index.html";
     });
 
 }
